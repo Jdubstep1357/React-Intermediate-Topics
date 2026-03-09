ececefd7e0abf391c8409854ce2a0206d773efff
John Lyons <jwlyons2015@gmail.com>
2026-03-09 15:03:05 -0400

    Fetching And Updating Data with React Query 18_Optimistic_Updates

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -3,22 +3,57 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Todo } from "./hooks/useTodos";
 import axios from "axios";
 
+interface AddTodoContext {
+  previousTodos: Todo[];
+}
+
 const TodoForm = () => {
   const queryClient = useQueryClient();
+
+  // optimistic update - assume server never down and cause fast updating right away
+
   // info on useMutation<1, 2, 3>: 1) Tdata - Todo object 2) TObject - Error, 3) T variable - Data sent to backend, ie: Todo
-  const addTodo = useMutation<Todo, Error, Todo>({
+  const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
     mutationFn: (todo: Todo) =>
       axios
         .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
         .then((res) => res.data),
-    onSuccess: (savedTodo, newTodo) => {
+
+    // Callbled before data is mutated
+    // optimistic update
+    onMutate: (newTodo: Todo) => {
+      // previous todos before update cache. if error, roll back to this
+      const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]) || [];
+
       queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
-        savedTodo,
+        newTodo,
         ...(todos || []),
       ]);
 
-      // CLEARS INPUT FIELD after submission
       if (ref.current) ref.current.value = "";
+
+      // this is what is rolled back if there is an error
+      return { previousTodos };
+    },
+
+    // 2 parameters - savedToDo from backend newTodo from client
+    // iterate over array and checks if it is the same as newtodo replace it with same todo, otherwise same todo object
+    onSuccess: (savedTodo, newTodo) => {
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
+        todos?.map((todo) =>
+          // after click, check if new todo is same as saved todo, replace it with saved todo
+          todo === newTodo ? savedTodo : todo,
+        ),
+      );
+    },
+
+    // IF FAIL
+    // 3rd parameter is context of type of type unknown
+    onError: (error, newTodo, context) => {
+      if (!context) return;
+
+      // if failed display error message and roll back to previous state
+      queryClient.setQueryData<Todo[]>(["todos"], context.previousTodos);
     },
   });
 
